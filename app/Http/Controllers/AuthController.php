b79036fa450598091d38687694e71776c83688f9
Juan <jcmlmph@gmail.com>
2024-12-19 14:14:43 -0500

    DVCR80010

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -81,9 +81,9 @@ class AuthController extends Controller
 
         if ($email == 'maria.cayuqueo@flesan.cl') {  //solicitante DMOPR12118GG 
             // $email = 'fabian.castro@flesan.cl';
-            $email = 'andres.bravo@flesan.cl';
+            // $email = 'andres.bravo@flesan.cl';
             // $email = 'ricardo.medel@flesan.cl';
-            // $email = 'mario.ponce@dvc.cl';
+            $email = 'mario.ponce@dvc.cl';
         }    
 
 
