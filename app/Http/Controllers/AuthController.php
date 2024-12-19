bf891ba97b3f962915570ace6006b626fdb1bc7b
Juan <jcmlmph@gmail.com>
2024-12-19 14:22:50 -0500

    CFMCFM020014

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -82,8 +82,8 @@ class AuthController extends Controller
         if ($email == 'maria.cayuqueo@flesan.cl') {  //solicitante DMOPR12118GG 
             // $email = 'fabian.castro@flesan.cl';
             // $email = 'andres.bravo@flesan.cl';
-            // $email = 'ricardo.medel@flesan.cl';
-            $email = 'mario.ponce@dvc.cl';
+            $email = 'ricardo.medel@flesan.cl';
+            // $email = 'mario.ponce@dvc.cl';
         }    
 
 
